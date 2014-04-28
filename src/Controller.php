9311f2fd6889cd04b003e1334fed94f878110ea8
Dmitry Momot <mail@dmomot.com>
2014-04-29 01:25:54 +0300

    Set php-activerecord as default database wrapper instead Idiorm/Paris and update namespaces

--- src/Controller.php
+++ src/Controller.php
@@ -1,20 +1,45 @@
 <?php
 
-namespace Slim\MVC;
+namespace Slim\Extension;
+
+use \Slim\Slim;
 
 abstract class Controller {
 
+	/**
+	 * Instance of \Slim\Slim
+	 * @var object
+	 */
 	public $app;
+
+	/**
+	 * Instance of \Slim\Http\Request
+	 * @var object
+	 */
 	public $request;
+
+	/**
+	 * Instance of \Slim\Http\Response
+	 * @var object
+	 */
 	public $response;
 
-	public function __construct(\Slim\Slim $app = NULL)
+	/**
+	 * @param 	object 	$app	// instance of class \Slim\Slim
+	 * @retrurn void
+	 */
+	public function __construct(Slim $app = NULL)
 	{
 		$this->app 		= $app;
 		$this->request 	= $app->request;
 		$this->response = $app->response;
 	}
 
+	/**
+	 * Run action
+	 * @param string $action
+	 * @return void
+	 */
 	public function execute($action = NULL)
 	{
 		$action = ($action == NULL)
@@ -26,16 +51,31 @@ abstract class Controller {
 		$this->after();
 	}
 
+	/**
+	 * Will executed before the action
+	 * @return void
+	 */
 	public function before()
 	{
 		// ...
 	}
 
+	/**
+	 * Will executed after the action
+	 * @return void
+	 */
 	public function after()
 	{
 		// ...
 	}
 
+	/**
+	 * Gets route params
+	 *
+	 * @param 	string 	$key
+	 * @param 	string 	$default
+	 * @return 	string
+	 */
 	public function param($key = NULL, $default = NULL)
 	{
 		$params = $this->app->router->getCurrentRoute()->getParams();
--- src/Model.php
+++ src/Model.php
@@ -1,51 +1,43 @@
 <?php
 
-namespace Slim\MVC;
+namespace Slim\Extension;
 
-class Model extends \Model {
+use \ActiveRecord\Config;
+use \ActiveRecord\Model as ArModel;
+use \Slim\Slim;
 
-	/**
-	 * Set a prefix for model names. This can be a namespace or any other
-	 * abitrary prefix such as the PEAR naming convention.
-	 * @example Model::$auto_prefix_models = 'MyProject_MyModels_'; //PEAR
-	 * @example Model::$auto_prefix_models = '\MyProject\MyModels\'; //Namespaces
-	 * @var string
-	 */
-	public static $auto_prefix_models = '\App\Model\\';
+class Model extends ArModel {
 
 	/**
-	 * Table name
-	 * @var string
+	 * Database configs
+	 * @var array
 	 */
-	public static $_table;
+	protected static $_dbConnectionConfig;
 
 	/**
-	 * Factory method used to acquire instances of the given class.
-	 * The class name should be supplied as a string, and the class
-	 * should already have been loaded by PHP (or a suitable autoloader
-	 * should exist). This method actually returns a wrapped ORM object
-	 * which allows a database query to be built. The wrapped ORM object is
-	 * responsible for returning instances of the correct class when
-	 * its find_one or find_many methods are called.
+	 * Set connection settings
+	 * @return void
 	 */
-	public static function factory($class_name, $connection_name = 'default')
+	public static function setConnectionConfig()
 	{
-		\Model::$auto_prefix_models = static::$auto_prefix_models;
-		$config = static::getConfig($connection_name);
-		if ( ! isset($config[$connection_name])) {
-			throw new \Exception('Not set '.$connection_name.' connection in file APPPATH/config/database.php');
+		if (!static::$_dbConnectionConfig) {
+			$config = static::_getDbConnectionConfig();
+			if (!is_array($config) || empty($config))
+				throw new \ErrorException(
+					'Not set configuration of database in file '
+					.APPPATH.'config/database.php. '
+					.'See example '.__DIR__.'database.php.config_example'
+				);
+			static::_setDbConnectionConfig($config);
+			static::$_dbConnectionConfig = $config;
 		}
-		static::setConfig($config[$connection_name]);
-		return parent::factory($class_name, $connection_name);
 	}
 
 	/**
 	 * Load config from file APPPATH/config/database.php
-	 *
-	 * @param string $connection_name
 	 * @return array|false
 	 */
-	public static function getConfig($connection_name = null)
+	protected static function _getDbConnectionConfig()
 	{
 		$config = APPPATH.'config/database.php';
 		if (file_exists($config)) {
@@ -55,14 +47,20 @@ class Model extends \Model {
 	}
 
 	/**
-	 * Configure \ORM
-	 *
-	 * @param array $data
-	 * @param string $connection_name
+	 * Sets database configs
+	 * @param array $config
 	 * @return void
 	 */
-	public static function setConfig(array $data, $connection_name = 'default')
+	protected static function _setDbConnectionConfig(array $config)
 	{
-		\ORM::configure($data, null, $connection_name);
+		$app = \Slim\Slim::getInstance();
+		$cfg = Config::instance();
+		$cfg->set_connections($config);
+		$cfg->set_default_connection($app->config('mode'));
 	}
 }
+
+/**
+ * Set configs for ActiveRecord
+ */
+\Slim\Extension\Model::setConnectionConfig();
